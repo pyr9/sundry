10de170f154090b53f02092235235c18a4fb85b0
panyuro <panyuro@163.com>
2024-05-14 11:15:23 +0800

    temp

---
+++ attahment/src/main/java/com/pyr/attahment/AttahmentApplication.java
@@ -0,0 +1,13 @@
+package com.pyr.attahment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttahmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttahmentApplication.class, args);
+    }
+
+}
---
+++ attahment/src/test/java/com/pyr/attahment/AttahmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyr.attahment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttahmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
